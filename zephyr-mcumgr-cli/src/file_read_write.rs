59021b1129958345fd22d9feb35010d677f8e1d2
Martin Stumpf <martin.stumpf@vected.de>
2025-12-01 11:44:31 +0100

    Beautification

--- zephyr-mcumgr-cli/src/file_read_write.rs
+++ zephyr-mcumgr-cli/src/file_read_write.rs
@@ -24,8 +24,8 @@ pub fn read_input_file(filename: &str) -> Result<(Box<[u8]>, Option<String>), Cl
 
         Ok((data.into_boxed_slice(), None))
     } else {
-        let file_path: &Path = filename.as_ref();
-        let mut file = File::open(file_path).map_err(CliError::InputReadFailed)?;
+        let filename: &Path = filename.as_ref();
+        let mut file = File::open(filename).map_err(CliError::InputReadFailed)?;
 
         let mut data = if let Ok(file_size) = file.metadata().map(|m| m.len() as usize) {
             Vec::with_capacity(file_size)
@@ -38,7 +38,7 @@ pub fn read_input_file(filename: &str) -> Result<(Box<[u8]>, Option<String>), Cl
 
         Ok((
             data.into_boxed_slice(),
-            file_path
+            filename
                 .file_name()
                 .map(|val| val.to_string_lossy().into_owned()),
         ))
