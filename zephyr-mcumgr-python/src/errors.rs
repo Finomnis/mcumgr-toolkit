5a316296a4675cb7129bc1e253b58ef9aa845d62
Finomnis <finomnis@gmail.com>
2026-01-29 22:23:39 +0100

    Make the error private

--- zephyr-mcumgr-python/src/errors.rs
+++ zephyr-mcumgr-python/src/errors.rs
@@ -3,7 +3,7 @@ use thiserror::Error;
 
 /// Possible Python library errors.
 #[derive(Error, Debug, Diagnostic)]
-pub enum McubootPythonError {
+pub(crate) enum McubootPythonError {
     #[error("Failed to find bootloader type '{1}'")]
     #[diagnostic(code(zephyr_mcumgr::python::parse_bootloader_type))]
     InvalidBootloaderString(#[source] strum::ParseError, String),
