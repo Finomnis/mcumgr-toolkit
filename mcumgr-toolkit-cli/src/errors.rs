7c624e20f2ea565141db405b74040fe3d2a51264
Finomnis <Finomnis@users.noreply.github.com>
2026-02-03 14:45:25 +0100

    Update diagnostic codes for CLI errors

--- mcumgr-toolkit-cli/src/errors.rs
+++ mcumgr-toolkit-cli/src/errors.rs
@@ -14,51 +14,51 @@ use mcumgr_toolkit::{
 #[derive(Error, Debug, Diagnostic)]
 pub enum CliError {
     #[error("Failed to open serial port")]
-    #[diagnostic(code(mcumgr_toolkit::cli::open_serial_failed))]
+    #[diagnostic(code(mcumgrctl::open_serial_failed))]
     OpenSerialFailed(#[source] serialport::Error),
     #[error("No backend selected")]
-    #[diagnostic(code(mcumgr_toolkit::cli::no_backend))]
+    #[diagnostic(code(mcumgrctl::no_backend))]
     NoBackendSelected,
     // #[error("Setting the timeout failed")]
-    // #[diagnostic(code(mcumgr_toolkit::cli::set_timeout_failed))]
+    // #[diagnostic(code(mcumgrctl::set_timeout_failed))]
     // SetTimeoutFailed(#[source] Box<dyn miette::Diagnostic + Send + Sync + 'static>),
     #[error("Command execution failed")]
-    #[diagnostic(code(mcumgr_toolkit::cli::execution_failed))]
+    #[diagnostic(code(mcumgrctl::execution_failed))]
     CommandExecutionFailed(#[from] ExecuteError),
     #[error("Json encode failed")]
-    #[diagnostic(code(mcumgr_toolkit::cli::json_encode))]
+    #[diagnostic(code(mcumgrctl::json_encode))]
     JsonEncodeError(#[source] serde_json::Error),
     #[error("Shell command returned error exit code: {}", Errno::errno_to_string(*.0))]
-    #[diagnostic(code(mcumgr_toolkit::cli::shell_exit_code))]
+    #[diagnostic(code(mcumgrctl::shell_exit_code))]
     ShellExitCode(i32),
     #[error("Failed to read the input data")]
-    #[diagnostic(code(mcumgr_toolkit::cli::input))]
+    #[diagnostic(code(mcumgrctl::input))]
     InputReadFailed(#[source] std::io::Error),
     #[error("Failed to write the output data")]
-    #[diagnostic(code(mcumgr_toolkit::cli::output))]
+    #[diagnostic(code(mcumgrctl::output))]
     OutputWriteFailed(#[source] std::io::Error),
     #[error("Unable to determine output file name")]
-    #[diagnostic(code(mcumgr_toolkit::cli::destination_unknown))]
+    #[diagnostic(code(mcumgrctl::destination_unknown))]
     DestinationFilenameUnknown,
     #[error("File upload failed")]
-    #[diagnostic(code(mcumgr_toolkit::cli::file_upload))]
+    #[diagnostic(code(mcumgrctl::file_upload))]
     FileUploadFailed(#[from] FileUploadError),
     #[error("File download failed")]
-    #[diagnostic(code(mcumgr_toolkit::cli::file_download))]
+    #[diagnostic(code(mcumgrctl::file_download))]
     FileDownloadFailed(#[from] FileDownloadError),
     #[error("Image upload failed")]
-    #[diagnostic(code(mcumgr_toolkit::cli::image_upload))]
+    #[diagnostic(code(mcumgrctl::image_upload))]
     ImageUploadFailed(#[from] ImageUploadError),
     #[error("Failed to parse datetime string")]
-    #[diagnostic(code(mcumgr_toolkit::cli::chrono_parse))]
+    #[diagnostic(code(mcumgrctl::chrono_parse))]
     ChronoParseFailed(#[from] chrono::ParseError),
     #[error("Failed to open USB serial port")]
-    #[diagnostic(code(mcumgr_toolkit::cli::usb_serial))]
+    #[diagnostic(code(mcumgrctl::usb_serial))]
     UsbSerialOpenFailed(#[from] UsbSerialError),
     #[error("Failed to parse MCUboot image")]
-    #[diagnostic(code(mcumgr_toolkit::cli::image_parse))]
+    #[diagnostic(code(mcumgrctl::image_parse))]
     ImageParseFailed(#[from] ImageParseError),
     #[error("Firmware update failed")]
-    #[diagnostic(code(mcumgr_toolkit::cli::firmware_update))]
+    #[diagnostic(code(mcumgrctl::firmware_update))]
     FirmwareUpdateFailed(#[from] FirmwareUpdateError),
 }
