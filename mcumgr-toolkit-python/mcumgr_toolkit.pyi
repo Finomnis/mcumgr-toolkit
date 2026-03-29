0392c0c35da90292940bece0658c15b90959f6e8
Finomnis <Finomnis@users.noreply.github.com>
2026-03-29 23:50:43 +0200

    Update dependency versions (#135)

--- mcumgr-toolkit-python/mcumgr_toolkit.pyi
+++ mcumgr-toolkit-python/mcumgr_toolkit.pyi
@@ -157,7 +157,7 @@ class MCUmgrClient:
     A high-level client for Zephyr's MCUmgr SMP functionality
     """
     @staticmethod
-    def serial(serial: builtins.str, baud_rate: builtins.int = 115200, timeout_ms: builtins.int = 500) -> MCUmgrClient:
+    def serial(serial: builtins.str, baud_rate: builtins.int = 115200, timeout_ms: builtins.int = 1000) -> MCUmgrClient:
         r"""
         Creates a new serial port based Zephyr MCUmgr SMP client.
         
@@ -168,7 +168,7 @@ class MCUmgrClient:
         * `timeout_ms` - The communication timeout, in ms.
         """
     @staticmethod
-    def usb_serial(identifier: builtins.str, baud_rate: builtins.int = 115200, timeout_ms: builtins.int = 500) -> MCUmgrClient:
+    def usb_serial(identifier: builtins.str, baud_rate: builtins.int = 115200, timeout_ms: builtins.int = 1000) -> MCUmgrClient:
         r"""
         Creates a Zephyr MCUmgr SMP client based on a USB serial port identified by VID:PID.
         
